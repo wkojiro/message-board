ad3b6d26a46c791ad28aa09a3e77cdd44d092009
wkojiro1 <wkojiro@eazydrive.co.jp>
2015-08-29 08:28:35 +0000

    CSS fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825092530) do
+ActiveRecord::Schema.define(version: 20150829080043) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
