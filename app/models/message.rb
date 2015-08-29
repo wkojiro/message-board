563369943638ef66c6c53c0971754c25a3c74e7f
wkojiro1 <wkojiro@eazydrive.co.jp>
2015-08-29 09:06:43 +0000

    age field add

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ActiveRecord::Base
     #名前は入力必須　且つ　20文字以内
     validates :name , length:{ maximum: 20} ,presence:true
-    #内容は入力必須　且つ　2文字以上30文字以下
+    #内容は入力必須　且つ　2文字以上30文字以
     validates :body , length:{minimum:2 , maximum: 30},presence:true
 end
