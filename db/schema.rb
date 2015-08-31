021dee482674e74e0fa7c6e8df86ba6c8b3560ed
wkojiro1 <wkojiro@eazydrive.co.jp>
2015-08-31 01:21:26 +0000

    merge conflict fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150829080043) do
+ActiveRecord::Schema.define(version: 20150829102722) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
